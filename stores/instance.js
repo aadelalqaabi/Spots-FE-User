de348fc74ab99cd0f1b8057bf4d067ea7b1a6cdf
AB <aasharq99@gmail.com>
2023-06-30 19:30:50 +0300

    seperated dev enviroments

--- stores/instance.js
+++ stores/instance.js
@@ -1,8 +1,19 @@
 import axios from "axios";
-
 import { TEST } from "../config/info";
-const baseURL = "https://destkw.com/api";
-//const baseURL = TEST;
+
+
+if (process.env.NODE_ENV === 'development') {
+  console.log('development')
+} else if (process.env.NODE_ENV === 'production') {
+  console.log('production')
+} else {
+  console.log('nope')
+}
+
+
+const baseURL = process.env.NODE_ENV === 'production' 
+  ? 'https://destkw.com/api' 
+  : TEST;
 
 const instance = axios.create({
   baseURL: baseURL,
